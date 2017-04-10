3bc4dd719ee7d3cfd611584b663056a5253064a1
Stephanie Courtney <scourtn4@ie.ibm.com>
2017-04-10 13:11:24 +0100

    added props and getDefaultProps

--- HelloReact/public/app.jsx
+++ HelloReact/public/app.jsx
@@ -1,13 +1,26 @@
 var Greeter = React.createClass({
+    // Default props will load when no value has been passed in
+    getDefaultProps: function() {
+        return {
+            name: 'React',
+            message: 'This is from a component'
+        }
+    },
     render: function(){
+        var name = this.props.name;
+        var message = this.props.message;
         return (
             <div>
-                <h1>Hello React!</h1>
-                <p>This is from the component</p>
+                <h1>Hello {name}!</h1>
+                <p>{message}</p>
             </div>
         );
     }
 });
 
+var firstName = 'Steph';
+var greetMessage = 'How are you?';
+
 ReactDOM.render(
-    <Greeter/>, document.getElementById('app'));
+    <Greeter name={firstName} message={greetMessage}/>, 
+    document.getElementById('app'));
