b9f16871f44594b323c77adacdd05b0e711cc899
Stephanie Courtney <scourtn4@ie.ibm.com>
2017-04-11 14:57:11 +0100

    added Main and Nav components

--- ReactWeather/app/app.jsx
+++ ReactWeather/app/app.jsx
@@ -1,5 +1,14 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var Main = require('Main');
 
 ReactDOM.render(
-    <h1>Boilerplate app!</h1>, document.getElementById('app'));
+    // Expects history prop
+    <Router history={hashHistory}>
+        <Route path="/" component={Main}>
+
+        </Route>
+    </Router>, 
+    document.getElementById('app')
+);
