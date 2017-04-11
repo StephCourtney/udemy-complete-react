8b1ce44f712d96d9bc3dd0fd55a0ebbd652be9a8
Stephanie Courtney <scourtn4@ie.ibm.com>
2017-04-11 13:27:58 +0100

    added babel stage-0

--- ReactBoilerplate/app/app.jsx
+++ ReactBoilerplate/app/app.jsx
@@ -1,6 +1,17 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var objOne = {
+    name: 'Steph',
+    location: 'Dublin'
+};
+
+var objTwo = {
+    age: 28,
+    ...objOne
+};
+
+console.log(objTwo);
 
 ReactDOM.render(
     <h1>Boilerplate app!</h1>, document.getElementById('app'));
